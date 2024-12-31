a4beb9702e44c1c57e13c80b3b7e82dfe4562d54
Francisco Santana <bragasan34@gmail.com>
2024-12-31 00:18:01 -0400

    Configuração do banco de dados PostgreSQL para produção

--- MeuTicket/settings.py
+++ MeuTicket/settings.py
@@ -84,13 +84,22 @@ WSGI_APPLICATION = 'MeuTicket.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'meuticket'),  # Nome do banco de dados
+        'USER': os.environ.get('DB_USER', 'meuticket_user'),  # Nome de usuário
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'bPkgxMlmYUjYw2mTFjofNgpIlev1ZHdN'),  # Senha
+        'HOST': os.environ.get('DB_HOST', 'dpg-ctpmsqi3esus73djalsg-a.oregon-postgres.render.com'),  # Host
+        'PORT': os.environ.get('DB_PORT', '5432'),  # Porta
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
